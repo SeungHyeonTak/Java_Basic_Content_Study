2ac182915860f7b3c54a91f388f7cd98de31a8b7
SeungHyeonTak-sub <conficker77@gmail.com>
2021-10-12 01:35:24 +0900

    - 디자인 패턴 facade 예시 2

---
+++ src/DesignPattern/facade/Ex2/Main.java
@@ -0,0 +1,10 @@
+package DesignPattern.facade.Ex2;
+
+import DesignPattern.facade.Ex2.system.Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.process();
+    }
+}
---
+++ src/DesignPattern/facade/Ex2/system/HelpSystem02.java
@@ -0,0 +1,11 @@
+package DesignPattern.facade.Ex2.system;
+
+class HelpSystem02 {
+    public HelpSystem02() {
+        System.out.println("Call Constructor : " + getClass().getSimpleName());
+    }
+
+    public void process() {
+        System.out.println("Call Process : " + getClass().getSimpleName());
+    }
+}
