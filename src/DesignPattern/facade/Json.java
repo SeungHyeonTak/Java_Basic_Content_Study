3e0002fefe3a80cd4c231c4bfe7c4d2d93b25832
SeungHyeonTak-sub <conficker77@gmail.com>
2021-10-12 01:24:44 +0900

    - 디자인 패턴 Facade 예제1

---
+++ src/DesignPattern/facade/Json.java
@@ -0,0 +1,12 @@
+package DesignPattern.facade;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Json {
+    public Map<String, Object> parse(String str) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("address", "서울시 강남구 삼성동");
+        return result;
+    }
+}
---
+++ src/DesignPattern/facade/Main.java
@@ -0,0 +1,7 @@
+package DesignPattern.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        new MyLocFacade().printMyAddress();
+    }
+}
